384f017fde5ca58f195422713e3906d4c66a667e
Kamesh <kamesh.sampath@hotmail.com>
2022-08-12 17:41:34 +0530

    escape envsubst

--- k8s/registry/app/test.sh
+++ k8s/registry/app/test.sh
@@ -1,13 +1,13 @@
-#!/bin/bash
+#!/bin/sh
 
 set -euxo pipefail
 
 # Internal 
 docker pull gcr.io/google-samples/hello-app:1.0
-docker tag gcr.io/google-samples/hello-app:1.0 "${MY_REGISTRY_URL}/example/hello-app:1.0"
-docker push  "${MY_REGISTRY_URL}/example/hello-app:1.0"
+docker tag gcr.io/google-samples/hello-app:1.0 "${DOLLAR}MY_REGISTRY_URL/example/hello-app:1.0"
+docker push  "${DOLLAR}MY_REGISTRY_URL/example/hello-app:1.0"
 
 # External
 docker pull gcr.io/google-samples/hello-app:1.0
-docker tag gcr.io/google-samples/hello-app:1.0 "${MY_REGISTRY_EXTERNAL_URL}/example/hello-app:1.0"
-docker push  "${MY_REGISTRY_EXTERNAL_URL}/example/hello-app:1.0"
+docker tag gcr.io/google-samples/hello-app:1.0 "${DOLLAR}MY_REGISTRY_EXTERNAL_URL/example/hello-app:1.0"
+docker push  "${DOLLAR}MY_REGISTRY_EXTERNAL_URL/example/hello-app:1.0"
