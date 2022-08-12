75f87775828087e16602e65d00839c5b07e948c7
Kamesh <kamesh.sampath@hotmail.com>
2022-08-12 10:21:59 +0530

    use fqn for registry

--- k8s/registry/test.sh
+++ k8s/registry/test.sh
@@ -2,7 +2,7 @@
 
 set -euxo pipefail
 
-local_reg="$MY_REGISTRY_URL:5000"
+local_reg="$MY_REGISTRY_URL"
 img_name="$local_reg/example/hello-app:1.0"
 docker pull gcr.io/google-samples/hello-app:1.0
 docker tag gcr.io/google-samples/hello-app:1.0 "$img_name"
